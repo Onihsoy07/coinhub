5dc5b7d307e210d72e77ea589535da4a929817b7
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-04-08 23:33:12 +0900

    테스트 : 업비트에서 공통 코인 buyList 구현

--- src/test/java/com/example/coinhub/service/UpbitMarketServiceTest.java
+++ src/test/java/com/example/coinhub/service/UpbitMarketServiceTest.java
@@ -1,21 +1,23 @@
 package com.example.coinhub.service;
 
+import com.example.coinhub.dto.CoinBuyDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 
 import java.util.List;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-// WireMock서버용 포트 열기
 //@AutoConfigureWireMock(port = 0)
-class BithumbMarketServiceTest {
+class UpbitMarketServiceTest {
 
     @Autowired
-    MarketService bithumbMarketService;
+    MarketService upbitMarketService;
 
     @Test
     void calculateBuy() {
@@ -25,17 +27,17 @@ class BithumbMarketServiceTest {
                 "ETH",
                 "ETC"
         );
-        double amount = 10000000D;
+        double amount = 100000000D;
         // doubl 값 계산으로 orderBook의 합이 정확한 amount가 나오지 않음. 약간의 오차 범위 설정
         double upperLimit = amount * 1.001;
         double lowerLimit = amount * 0.999;
 
         // when
-        Map<String, Map<Double, Double>> orderBooks = bithumbMarketService.calculateBuy(commonCoin, amount).getOrderBooks();
+        Map<String, Map<Double, Double>> orderBookList = upbitMarketService.calculateBuy(commonCoin, amount).getOrderBooks();
 
         // then
-        for (String key : orderBooks.keySet()) {
-            Map<Double, Double> coinOrderBook = orderBooks.get(key);
+        for (String coin : orderBookList.keySet()) {
+            Map<Double, Double> coinOrderBook = orderBookList.get(coin);
             double totalPrice = 0D;
             for (Double price : coinOrderBook.keySet()) {
                 Double quantity = coinOrderBook.get(price);
@@ -43,5 +45,6 @@ class BithumbMarketServiceTest {
             }
             assertThat(totalPrice).isGreaterThanOrEqualTo(lowerLimit).isLessThanOrEqualTo(upperLimit);
         }
+
     }
 }
