dbd3f046f376a2c6f6235ff0b292db35eee2294f
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-04-14 21:55:47 +0900

    테스트 : (임시) 빗썸 출금 수수료 크롤링
    Vue? React? 아마 Vue로 구현 된 프론트 같은데 div root 내부 태그 안나옴;;;;;

--- src/test/java/com/example/coinhub/service/BithumbMarketServiceTest.java
+++ src/test/java/com/example/coinhub/service/BithumbMarketServiceTest.java
@@ -1,16 +1,20 @@
 package com.example.coinhub.service;
 
 import com.example.coinhub.dto.CoinBuyDto;
-import com.example.coinhub.dto.CoinSellDto;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
 
+//@Disabled
+@EnableFeignClients
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 // WireMock서버용 포트 열기
 //@AutoConfigureWireMock(port = 0)
@@ -72,6 +76,12 @@ class BithumbMarketServiceTest {
             assertThat(totalCoin).isEqualTo(coinAmount);
         }
 
+    }
+
+    @Test
+    void calculateTransferFee() throws IOException {
+        Map<String, Double> transferFee = bithumbMarketService.calculateTransferFee();
+        assertThat(transferFee).isNotEmpty();
     }
 
 
